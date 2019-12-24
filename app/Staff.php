a1623e503d39bf8f5a8be0786843ebe6b8dfa724
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2019-12-24 17:01:37 +0000

    first commit

---
+++ app/Staff.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    //
+    public function photos(){
+
+        return $this->morphMany('App\Photo', 'imageable');
+
+    }
+}
